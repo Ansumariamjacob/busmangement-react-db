af1400585c23d40370d552a755aab7b3769c6a90
ansumariamjacob <ansumariam99@gmail.com>
2022-05-25 16:52:37 +0530

    Bus managementSearch

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Addbus from './component/Addbus';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Searchbus from './component/Searchbus';
 
 function App() {
   return (
     <div className="App">
       <Addbus/>
+      <Searchbus/>
     </div>
   );
 }
--- src/component/Searchbus.js
+++ src/component/Searchbus.js
@@ -1,4 +1,14 @@
-<div class="container">
+import React, { useState } from 'react'
+
+const Searchbus = () => {
+    var [regno,setRegno]=useState("")
+    const serData=()=>{
+        const data={"registration number":regno}
+        console.log(data)
+    }
+  return (
+    <div>
+        <div class="container">
     <div class="row">
         <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <div class="row g-3">
@@ -6,12 +16,17 @@
                     <label for="" class="form-label">Bus Registration Number</label>
                 </div>
                 <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <input type="text" class="form-control">
+                    <input onChange={(e)=>{setRegno(e.target.value)}} type="text" class="form-control"/>
                 </div>
                 <div class="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <button class="btn btn-success">Search</button>
+                    <button onClick={serData} class="btn btn-success">Search</button>
                 </div>
             </div>
         </div>
     </div>
 </div>
+    </div>
+  )
+}
+
+export default Searchbus
